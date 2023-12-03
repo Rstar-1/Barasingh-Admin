ba4dc274aea8028a44e3b1e9cd08122f9e488263
Rstar-1 <rajshetye.5855@gmail.com>
2023-12-03 14:19:11 +0530

    sidebar

--- src/pages/admin/sidebar/Sidebar.js
+++ src/pages/admin/sidebar/Sidebar.js
@@ -14,18 +14,38 @@ const Sidebar = () => {
   const sidebarmenu2 = [
     {
       id: 1,
-      name: "Subscription",
-      icon: "file",
+      name: "Plans",
+      icon: "truck",
       route: "subscription",
     },
     {
       id: 2,
-      name: "Projects",
-      icon: "search",
+      name: "Plan Users",
+      icon: "pocket",
       route: "project",
     },
   ];
   const sidebarmenu3 = [
+    {
+      id: 1,
+      name: "Projects",
+      icon: "layout",
+      route: "project",
+    },
+    {
+      id: 2,
+      name: "Brands",
+      icon: "aperture",
+      route: "subscription",
+    },
+    {
+      id: 3,
+      name: "Reviews",
+      icon: "star",
+      route: "subscription",
+    },
+  ];
+  const sidebarmenu4 = [
     {
       id: 1,
       name: "CRM",
@@ -51,7 +71,7 @@ const Sidebar = () => {
       route: "ecommerce",
     },
   ];
-  const sidebarmenu4 = [
+  const sidebarmenu5 = [
     {
       id: 1,
       name: "Cms",
@@ -66,12 +86,18 @@ const Sidebar = () => {
     },
     {
       id: 3,
+      name: "Addons",
+      icon: "shopping-bag",
+      route: "addons",
+    },
+    {
+      id: 4,
       name: "Billing",
       icon: "book",
       route: "billing",
     },
     {
-      id: 4,
+      id: 5,
       name: "Get the App",
       icon: "tablet",
       route: "getapp",
@@ -92,7 +118,7 @@ const Sidebar = () => {
               <input type="checkbox" name="accordion-1" id="cb1" />
               <label
                 for="cb1"
-                className="p10 hover-col cursor-pointer flex justify-between items-center fsize15 textforth mtpx2 font-300"
+                className="p10 hover-col cursor-pointer flex justify-between items-center textforth font-300"
               >
                 <div className="flex items-center gap-4">
                   <FeatherIcon
@@ -100,7 +126,7 @@ const Sidebar = () => {
                     className="textgray cursor-pointer"
                     size={16}
                   />
-                  Home
+                  <p className="fsize14 mtpx3">Home</p>
                 </div>
                 <div className="flex items-center gap-4">
                   <p className="fsize12 dot-dash rounded-full flex items-center justify-center textwhite bgprimary">
@@ -120,9 +146,9 @@ const Sidebar = () => {
                       <FeatherIcon
                         icon={e.icon}
                         className=" cursor-pointer"
-                        size={16}
+                        size={15}
                       />
-                      <p className="fsize14  mtpx2 font-300">{e.name}</p>
+                      <p className="fsize13 mtpx2 font-300">{e.name}</p>
                     </NavLink>
                   ))}
                 </div>
@@ -132,7 +158,7 @@ const Sidebar = () => {
               <input type="checkbox" name="accordion-2" id="cb2" />
               <label
                 for="cb2"
-                className="p10 hover-col cursor-pointer flex justify-between items-center fsize15 textforth mtpx2 font-300"
+                className="p10 hover-col cursor-pointer flex justify-between items-center textforth  font-300"
               >
                 <div className="flex items-center gap-4">
                   <FeatherIcon
@@ -140,7 +166,7 @@ const Sidebar = () => {
                     className="textgray cursor-pointer"
                     size={16}
                   />
-                  Website
+                  <p className="fsize14 mtpx3">Subscription</p>
                 </div>
                 <div className="flex items-center gap-4">
                   <p className="fsize12 dot-dash rounded-full flex items-center justify-center textwhite bgprimary">
@@ -160,9 +186,9 @@ const Sidebar = () => {
                       <FeatherIcon
                         icon={e.icon}
                         className=" cursor-pointer"
-                        size={16}
+                        size={15}
                       />
-                      <p className="fsize14  mtpx2 font-300">{e.name}</p>
+                      <p className="fsize13 mtpx2 font-300">{e.name}</p>
                     </NavLink>
                   ))}
                 </div>
@@ -172,15 +198,15 @@ const Sidebar = () => {
               <input type="checkbox" name="accordion-3" id="cb3" />
               <label
                 for="cb3"
-                className="p10 hover-col cursor-pointer flex justify-between items-center fsize15 textforth mtpx2 font-300"
+                className="p10 hover-col cursor-pointer flex justify-between items-center textforth font-300"
               >
                 <div className="flex items-center gap-4">
                   <FeatherIcon
-                    icon="briefcase"
+                    icon="file"
                     className="textgray cursor-pointer"
                     size={16}
                   />
-                  Management
+                  <p className="fsize14 mtpx3">Website</p>
                 </div>
                 <div className="flex items-center gap-4">
                   <p className="fsize12 dot-dash rounded-full flex items-center justify-center textwhite bgprimary">
@@ -200,9 +226,49 @@ const Sidebar = () => {
                       <FeatherIcon
                         icon={e.icon}
                         className=" cursor-pointer"
-                        size={16}
+                        size={15}
                       />
-                      <p className="fsize14  mtpx2 font-300">{e.name}</p>
+                      <p className="fsize13 mtpx2 font-300">{e.name}</p>
+                    </NavLink>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div class="tab">
+              <input type="checkbox" name="accordion-4" id="cb4" />
+              <label
+                for="cb4"
+                className="p10 hover-col cursor-pointer flex justify-between items-center textforth font-300"
+              >
+                <div className="flex items-center gap-4">
+                  <FeatherIcon
+                    icon="briefcase"
+                    className="textgray cursor-pointer"
+                    size={16}
+                  />
+                  <p className="fsize14 mtpx3">Management</p>
+                </div>
+                <div className="flex items-center gap-4">
+                  <p className="fsize12 dot-dash rounded-full flex items-center justify-center textwhite bgprimary">
+                    {sidebarmenu4.length}
+                  </p>
+                  <FeatherIcon size="16" icon="chevron-right" />
+                </div>
+              </label>
+              <div class="tab__content">
+                <div className="grid grid-cols-1 p9 gap-12">
+                  {sidebarmenu4.map((e) => (
+                    <NavLink
+                      activeClassName="active"
+                      className="flex items-center activesidebar pbpx7 gap-9"
+                      to={e.route}
+                    >
+                      <FeatherIcon
+                        icon={e.icon}
+                        className=" cursor-pointer"
+                        size={15}
+                      />
+                      <p className="fsize13 mtpx2 font-300">{e.name}</p>
                     </NavLink>
                   ))}
                 </div>
@@ -210,7 +276,7 @@ const Sidebar = () => {
             </div>
           </div>
           <div className="grid grid-cols-1 p9 gap-12">
-            {sidebarmenu4.map((e) => (
+            {sidebarmenu5.map((e) => (
               <NavLink
                 activeClassName="active"
                 className="flex items-center activesidebar pbpx7 gap-9"
@@ -219,9 +285,9 @@ const Sidebar = () => {
                 <FeatherIcon
                   icon={e.icon}
                   className=" cursor-pointer"
-                  size={16}
+                  size={15}
                 />
-                <p className="fsize14  mtpx2 font-300">{e.name}</p>
+                <p className="fsize13 mtpx2 font-300">{e.name}</p>
               </NavLink>
             ))}
           </div>
